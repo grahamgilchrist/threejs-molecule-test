5fe3198da8b85d15f5760ead7cace60c6ddafc51
Graham Gilchrist <graham@incuna.com>
2012-05-11 15:39:14 +0100

    Updated script to load external mesh file via JSON

--- scripts/init.js
+++ scripts/init.js
@@ -1,49 +1,56 @@
-(function () {
+$(document).ready(function() {
     var camera, 
-        scene, 
+        scene,
         renderer,
         geometry,
         material,
         mesh;
 
-    function init() {
+    var SCREEN_WIDTH = window.innerWidth;
+    var SCREEN_HEIGHT = window.innerHeight;
 
+    function init() {
         scene = new THREE.Scene();
 
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.z = 1000;
-        scene.add( camera );
+        camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+        camera.position.z = 100;
+        scene.add(camera);
 
-        geometry = new THREE.CubeGeometry( 200, 200, 200 );
-        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
-
+        // RENDERER
         renderer = new THREE.CanvasRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+
+        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false } );
+        //material = new THREE.MeshFaceMaterial();
+        var loader = new THREE.JSONLoader();
+        var callbackMale = function( geometry ) { 
+            mesh = new THREE.Mesh(geometry, material);
+            scene.add(mesh);
+
+            //call animate once this is loaded
+            animate();
+        };
+
+        //"data/Male02_slim.js", 
+        loader.load(
+            "data/WaltHeadLo.js", 
+            callbackMale 
+        );
 
         document.body.appendChild( renderer.domElement );
-
     }
 
     function animate() {
-
         // note: three.js includes requestAnimationFrame shim
         requestAnimationFrame( animate );
         render();
-
     }
 
     function render() {
-
         mesh.rotation.x += 0.01;
         mesh.rotation.y += 0.02;
-
         renderer.render( scene, camera );
-
     }
 
     init();
-    animate();
-})();
+});
